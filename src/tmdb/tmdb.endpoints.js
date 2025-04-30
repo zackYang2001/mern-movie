d3ebdc14102437404055219f321350fe5ddc3b66
ZackY-fontend <qq26283871@gmail.com>
2025-04-30 14:14:57 +0800

    新增資料庫連線及 api 設定

---
+++ src/tmdb/tmdb.endpoints.js
@@ -0,0 +1,36 @@
+import tmdbConfig from "./tmdb.config.js"
+
+const tmdbEndpoints = {
+    mediaList: ({ mediaType, mediaCategory, page }) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaCategory}`,page
+    ),
+    mediaDetail: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}`
+    ),
+    mediaGenres: ({ mediaType }) => tmdbConfig.getUrl(
+        `genre/${mediaType}/list`
+    ),
+    mediaVideos: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/credits`
+    ),
+    mediaCredits: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/videos`
+    ),
+    mediaRecommend: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/recommendations`
+    ),
+    mediaImages: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/images`
+    ),
+    mediaSearch: ({ mediaType, query, page }) => tmdbConfig.getUrl(
+        `search/${mediaType}`, { query, page}
+    ),
+    personDetail: ({ personId }) => tmdbConfig.getUrl(
+        `person/${personId}`
+    ),
+    personMedia: ({ personId }) => tmdbConfig.getUrl(
+        `person/${personId}/combined_credits`
+    ),
+}
+
+export default tmdbEndpoints
