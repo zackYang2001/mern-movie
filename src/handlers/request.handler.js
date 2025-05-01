46881a438a73859c6f363ee895c36d038c002526
ZackY-fontend <qq26283871@gmail.com>
2025-05-01 15:19:47 +0800

    處理請求與回應

---
+++ src/handlers/request.handler.js
@@ -0,0 +1,12 @@
+import { validationResult } from "express-validator";
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) return res.status(400).json(errors.array()[0].msg);
+  // isEmpty() 是 validationResult 提供用來檢查是否「沒有」錯誤
+
+  next()
+};
+
+export default { validate };
