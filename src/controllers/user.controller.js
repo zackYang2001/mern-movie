40dd3ed29c3bea7b93315d8dd45a695a2569b329
ZackY-fontend <qq26283871@gmail.com>
2025-05-02 13:48:39 +0800

    新增各功能的控制器

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,112 @@
+import userModel from "../models/user.model.js";
+import jsonwebtoken from "jsonwebtoken";
+import responseHandler from "../handlers/response.handler.js";
+
+const signup = async (req, res) => {
+  try {
+    const { username, password, displayName } = req.body;
+
+    const checkUser = await userModel.findOne({ username });
+
+    if (checkUser)
+      return responseHandler.badrequest(res, "Username already used");
+
+    const user = new userModel();
+
+    user.displayName = displayName;
+    user.username = username;
+    user.setPassword(password);
+
+    await user.save();
+
+    const token = jsonwebtoken.sign(
+      { data: user.id },
+      process.env.TOKEN_SECRET,
+      { expiresIn: "24h" }
+    );
+
+    responseHandler.created(res, {
+      token,
+      ...user._doc, // 把所有 user 資料（帳號、暱稱等）展開放進 response
+      // user 本身不只是資料，還有 mongoose 方法
+      // 所以 user._doc 是這個物件的「純資料內容」，也就是我存進的 mongoDB 的欄位資料
+      id: user.id, // 傳一個簡潔字串形式的 userId，前端較好處裡
+    });
+  } catch {
+    responseHandler.error(res);
+  }
+};
+
+const signin = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await userModel
+      .findOne({ username })
+      .select("username password salt id displayName"); // 選擇要顯示的欄位
+
+    if (!user) return responseHandler.badrequest(res, "User not exit");
+
+    if (!user.validPassword(password))
+      return responseHandler.badrequest(res, "Wrong password");
+
+    const token = jsonwebtoken.sign(
+      { data: user.id },
+      process.env.TOKEN_SECRET,
+      { expiresIn: "24h" }
+    );
+
+    // 清除 password, salt 敏感資料
+    user.password = undefined;
+    user.salt = undefined;
+
+    responseHandler.created(res, {
+      token,
+      ...user._doc,
+      id: user.id,
+    });
+  } catch {
+    responseHandler.error(res);
+  }
+};
+
+const getInfo = async (req, res) => {
+  try {
+    const user = await userModel.findById(req.user.id);
+
+    if (!user) return responseHandler.notfound(res);
+
+    responseHandler.ok(res, user);
+  } catch {
+    responseHandler.error(res);
+  }
+};
+
+const updatePassword = async (req, res) => {
+  try {
+    const { password, newPassword } = req.body;
+    const user = await userModel
+      .findById(req.user.id)
+      .select("password id salt");
+
+    if (!user) return responseHandler.unauthorize(res);
+
+    if (!user.validPassword(password))
+      return responseHandler.badrequest(res, "Wrong password");
+
+    user.setPassword(newPassword);
+
+    await user.save();
+
+    responseHandler.ok(res);
+  } catch {
+    responseHandler.error(res);
+  }
+};
+
+export default {
+  signup,
+  signin,
+  getInfo,
+  updatePassword,
+};
